993c35d374beffb3c978bd5b35972ce4aef9d9c6
Austin Whittington <austin_whitt@yahoo.com>
2015-12-09 18:22:15 -0600

    finished part 2 of day 6

--- day6.hs
+++ day6.hs
@@ -18,9 +18,6 @@ applySlice f start end v = a U.++ b U.++ c
           b = U.map f $ U.slice start (end - start + 1) v
           c = U.drop (end + 1) v
 
-lights :: String -> U.Vector Bool -> U.Vector Bool
-lights _ _ = U.empty
-
 unpack :: String -> [[String]]
 unpack s = s =~ "((?:[a-z]+ )+)([0-9]+),([0-9]+) [a-z]+ +([0-9]+),([0-9]+)"
 
@@ -39,5 +36,31 @@ day6 = do
     contents <- readFile "day6.txt"
     let lights = foldl (flip $ apply . tuplify . unpack) (U.replicate 1000 (U.replicate 1000 False)) $ lines contents
         numbers = U.map (U.foldl (\n b -> if b == True then n+1 else n) 0) lights
+        answer = U.sum numbers
+    putStrLn (show answer)
+
+toggle' :: Int -> Int
+toggle' = (+2)
+
+turnOn' :: Int -> Int
+turnOn' = (+1)
+
+turnOff' :: Int -> Int
+turnOff' n
+    | n > 0 = n-1
+    | otherwise = 0
+
+apply' :: (String, Int, Int, Int, Int) -> U.Vector (U.Vector Int) -> U.Vector (U.Vector Int)
+apply' (s, sx, sy, ex, ey) m = applySlice (applySlice f sx ex) sy ey m 
+    where 
+    f = case s of 
+        "turn off " -> turnOff'
+        "turn on " -> turnOn'
+        "toggle " -> toggle'
+
+day6' = do
+    contents <- readFile "day6.txt"
+    let lights = foldl (flip $ apply' . tuplify . unpack) (U.replicate 1000 (U.replicate 1000 0)) $ lines contents
+        numbers = U.map U.sum lights
         answer = U.sum numbers
     putStrLn (show answer)
