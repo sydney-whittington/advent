11200d34c23e7b450c187c9f55a9630e9de70e3f
Austin Whittington <austin_whitt@yahoo.com>
2015-12-14 23:16:27 -0600

    finished day 8

---
+++ day8.hs
@@ -0,0 +1,23 @@
+realCount :: String -> Int
+realCount [] = 0
+realCount ('"':xs) = 0 + realCount xs
+realCount ('\\':'\\':xs) = 1 + realCount xs
+realCount ('\\':'\"':xs) = 1 + realCount xs
+realCount ('\\':'x':_:_:xs) = 1 + realCount xs
+realCount (_:xs) = 1 + realCount xs
+
+day8 = do
+    contents <- readFile "day8.txt"
+    let strings = lines contents
+        chars = sum $ map length strings
+        realChars = sum $ map realCount strings
+        answer = chars - realChars
+    putStrLn (show answer)
+
+day8' = do
+    contents <- readFile "day8.txt"
+    let strings = lines contents
+        chars = sum $ map length strings
+        encodedChars = sum $ map (length . show) strings
+        answer = encodedChars - chars
+    putStrLn (show answer)
