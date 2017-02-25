1d131852b528e7fda90a3cfde6a1c77c75eb2bbd
Austin Whittington <austin_whitt@yahoo.com>
2017-02-25 00:07:40 -0600

    finished day 2 part 2

--- 2016/day2/Program.fs
+++ 2016/day2/Program.fs
@@ -48,31 +48,35 @@ module day2 =
 
     let key2 loc = keypad2.[loc.Y].[loc.X]
 
+    let safe2 =
+        function
+        | x when x < 0 -> 0
+        | x when x > 4 -> 4
+        | x -> x
+
     let motion2 loc i =
         let newLoc = i |> (fun i -> match i with 
-                                    | Some U -> {loc with Y = loc.Y - 1}
-                                    | Some L -> {loc with X = loc.X - 1}
-                                    | Some R -> {loc with X = loc.X + 1}
-                                    | Some D -> {loc with Y = loc.Y + 1}
+                                    | Some U -> {loc with Y = loc.Y - 1 |> safe2}
+                                    | Some L -> {loc with X = loc.X - 1 |> safe2}
+                                    | Some R -> {loc with X = loc.X + 1 |> safe2}
+                                    | Some D -> {loc with Y = loc.Y + 1 |> safe2}
                                     | None -> {X = 999; Y = 999})
-        printfn "%A" newLoc
         if key2 newLoc = 0
         then loc
         else newLoc
 
     let contents = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "/day2.txt")
-    let commands = contents.Split([|'\n'|], System.StringSplitOptions.RemoveEmptyEntries) |> Seq.map (Seq.map parse)
+    let commands = contents.Split([|"\r\n"; "\n"|], System.StringSplitOptions.RemoveEmptyEntries) |> Seq.map (Seq.map parse)
 
     // part 1
     let start = {X = 1; Y = 1}
     let locations = Seq.scan (Seq.fold motion) start commands
-    let digits = Seq.map key locations |> Seq.toList
     // skip the first digit, since it's the start point
-    printfn "day2: %A" digits.[1..]
+    let digits = Seq.map key locations |> Seq.map (sprintf "%X") |> Seq.tail |> String.concat ""
+    printfn "day2: %A" digits
 
     // part 2
-    let start2 = {X = 2; Y = 0}
+    let start2 = {X = 0; Y = 2}
     let locations2 = Seq.scan (Seq.fold motion2) start2 commands
-    let digits2 = Seq.map key2 locations |> Seq.toList
-    // skip the first digit, since it's the start point
-    printfn "day2': %A" digits2.[1..]
+    let digits2 = Seq.map key2 locations2 |> Seq.map (sprintf "%X") |> Seq.tail |> String.concat ""
+    printfn "day2': %A" digits2
