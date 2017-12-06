841bbe47ae1adcf4bc51868630cddc154db3a463
Austin Whittington <austin.whittington@swri.org>
2017-12-06 16:12:08 -0600

    added day 2 solution

--- advent/src/day2.hs
+++ advent/src/day2.hs
@@ -3,10 +3,23 @@
 module Day2 where
 
 import Data.List
---import Data.List.Split
+import Data.List.Split
 
---day2 = do
---    contents <- readFile "day2.txt"
---    let rows = lines contents
---        numbers = map (map read) $ map (splitAt "\t") rows
---    putStrLn (show numbers)
+checksum :: [Int] -> Int
+checksum xs = maximum xs - minimum xs
+
+day2 = do
+    contents <- readFile "src/inputs/day2.txt"
+    let rows = lines contents
+        answer = sum $ map (checksum . map read . splitOn "\t") rows
+    print answer
+
+divisible :: [Int] -> Int
+divisible xs = uncurry div pair
+    where pair = head [ (x, y) | x<-xs, y<-xs, x /= y, x `mod` y == 0]
+
+day2' = do
+    contents <- readFile "src/inputs/day2.txt"
+    let rows = lines contents
+        answer = sum $ map (divisible . map read . splitOn "\t") rows
+    print answer
